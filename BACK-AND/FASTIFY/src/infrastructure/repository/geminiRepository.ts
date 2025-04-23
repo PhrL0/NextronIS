fbbe43429e2d7a7b2927909dac95201a1652f314
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-22 21:45:14 -0300

    Criando memoria no gemini

--- BACK-AND/FASTIFY/src/infrastructure/repository/geminiRepository.ts
+++ BACK-AND/FASTIFY/src/infrastructure/repository/geminiRepository.ts
@@ -5,5 +5,21 @@ export const geminiRepository = {
     fetchAllRecords: async(query: string): Promise<any[]> =>{
         const result = await prisma.$queryRawUnsafe(String(query));
         return result as any[]; 
+    },
+    logAIInteraction: async(askUser: string,buildQuery: string,humanResponse: string,userFeedback : string): Promise<any> =>{
+        try{
+            const result = await prisma.interacaoIA.create({
+                data:{
+                    perguntaUsuario:askUser,
+                    queryMontada:buildQuery,
+                    respostaHumanizada:humanResponse,
+                    feedbackUsuario:userFeedback
+                }
+            });
+
+            return result;
+        } catch(err) {
+            console.error("Error",err);
+        }
     }
 }
